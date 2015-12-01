eba27ba07e8dcf4124bd99221d23a470996da0bc
jrue3084 <jrue3084@cox.net>
2015-12-01 10:31:39 -0800

    date formatting

--- app/models/article.js
+++ app/models/article.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   authorName: DS.attr('string'),
   npTitle: DS.attr('string'),
-  date: DS.attr('number'),
+  date: DS.attr('date'),
   content: DS.attr('string'),
   tags: DS.hasMany('tag', {async: false}),
 });
