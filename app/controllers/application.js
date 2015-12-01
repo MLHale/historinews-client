75b8a689fdab8599a7153e4cd40a49f4c3977e73
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-12-01 02:02:26 -0600

    Added search bar functionality

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,4 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   articles: {},
+  searchText: '',
+  filteredArticles: function () {
+    var filter = this.get('searchText');
+    var rx = new RegExp(filter, 'gi');
+    var articles = this.get('articles');
+
+    return articles.filter(function(article){
+          return article.get('title').match(rx)  || article.get('authorName').match(rx) ||
+                 article.get('npTitle').match(rx) || article.get('content').match(rx);
+        });
+  }.property('articles','searchText'),
+  actions: {
+    search: function(){
+      this.get('filteredArticles');
+    },
+  },
 });
