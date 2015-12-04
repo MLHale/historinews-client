9264a365889c21f2bc28eb06af91346755603120
Ryan Grandgenett <ryan.grandgenett@gmail.com>
2015-12-04 00:48:51 -0600

    Added working sort by dropdown menu

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,24 +6,18 @@ export default Ember.Controller.extend({
   filterOptions: {startDate: 1900, endData: 2000},
   searchFlag: false,
 
-  newspapersYearSorting: ['newspaperYear:desc'],
-  newspapersAuthorSorting: ['authorName'],
-  
-  sortednewspapersYear: Ember.computed.sort('posts', 'newspapersYearSorting'),
-  sortednewspapersAuthor: Ember.computed.sort('posts', 'newspapersAuthorSorting'),
+  sortProperty: ['newspaperTitle'],  
+  sortedNewspapers: Ember.computed.sort('newspapers', 'sortProperty'),
 
   actions: {
     search: function(){
       var newspapers = this.store.query('newspaper', this.get('searchOptions'));
       this.set('newspapers', newspapers);
       this.set('searchFlag', true);
-      console.log(this.get('sortednewspapersYear'));
-
     },
     selectFilter: function(selection, component) { 
       var searchOptions = this.get('searchOptions');
       
-      // Set the filter selection object
       for (var prop in searchOptions) {
         if (prop === selection) {
           searchOptions[prop] = true;
@@ -32,6 +26,9 @@ export default Ember.Controller.extend({
           searchOptions[prop] = false;
         } 
       }
+    },
+    selectSort: function(selection, component){
+      this.set('sortProperty', [selection]);
     },
   },
 });
