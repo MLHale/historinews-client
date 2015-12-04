d8d4c0c98aee27f2e32877abf169b0cc225dc173
bhealy <brianhealy18@yahoo.com>
2015-12-04 14:09:47 -0800

    Sidebar submit button now queries all information selected and is ready to submit it to server

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,13 +5,13 @@ export default Ember.Controller.extend({
   newspapers: {},
   searchText: '',
   searchOptions: {all: true, newspaperTitle: false, authorName: false, newspaperName: false,newspaperCreationDate: false, newspaperYear: false, keywords: false, ocrText: false,},
-  filterOptions: {startDate: 1900, endData: 2000},
+  filterOptions: {startDate: "", endDate: "", name: "", period: ""},
   searchFlag: false,
 
   sortProperty: ['newspaperTitle'],  
   sortedNewspapers: Ember.computed.sort('newspapers', 'sortProperty'),
-  newspaperNames: Ember.computed.mapBy('sortedNewspapers', 'newspaperName'),
-  uniqueNewspaperNames: Ember.computed.uniq('newspaperNames'),
+  newspaperNames: Ember.computed.mapBy('sortedNewspapers', 'newspaperName'), //uses the sorted values to find all Newspaper Names
+  uniqueNewspaperNames: Ember.computed.uniq('newspaperNames'), //finds all unique Newspaper Name values
 
   // setup our query params
   queryParams: ["page", "perPage"],
@@ -30,10 +30,34 @@ export default Ember.Controller.extend({
   totalPagesBinding: "pagedNewsPapers.totalPages",
 
   actions: {
+    filterResults: function(){
+      var checkedNewspaperNames = $('.NewspaperName:checkbox:checked').map(function() { //Finds all selected Newspaper Names
+          return this.value;
+      }).get();
+      checkedNewspaperNames = checkedNewspaperNames.join("|"); //deliminated by |
+      this.set('filterOptions.name', checkedNewspaperNames);
+
+      var checkedPeriods = $('.Period:checkbox:checked').map(function() { //Finds all selected Period times
+          return this.value;
+      }).get();
+      checkedPeriods = checkedPeriods.join("|"); //deliminated by |
+      this.set('filterOptions.period', checkedPeriods);
+
+      var start = $('#startDate').val(); 
+      if (start) //if the value is filled in
+        this.set('filterOptions.startDate', start)
+
+      var end = $('#endDate').val();
+      if (end) //if the value is filled in
+        this.set('filterOptions.endDate', end)
+
+      console.log(this.filterOptions); //woo, it works
+    },
     search: function(){
       var newspapers = this.store.query('newspaper', this.get('searchOptions'));
       this.set('newspapers', newspapers);
       this.set('searchFlag', true);
+      Ember.$(":checkbox").prop("checked", false); // Resets all checkboxes whenever something is searched for
     },
     selectFilter: function(selection, component) { 
       var searchOptions = this.get('searchOptions');
